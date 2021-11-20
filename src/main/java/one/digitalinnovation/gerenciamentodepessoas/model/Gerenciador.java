eeb9d1b1e37e5de68b065b323d6a6ad3e6e76ce9
tholivera <emaildathaisoliveira.silva@gmail.com>
2021-11-20 18:50:17 -0300

    Atualização

--- src/main/java/one/digitalinnovation/gerenciamentodepessoas/model/Gerenciador.java
+++ src/main/java/one/digitalinnovation/gerenciamentodepessoas/model/Gerenciador.java
@@ -1,5 +1,22 @@
 package one.digitalinnovation.gerenciamentodepessoas.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tb_gerenciador")
 public class Gerenciador {
+	
+	
+	
+	private long id;
+	
+	
+	
+	private String nome;
+	
+	
+	
+	private String 
 
 }
