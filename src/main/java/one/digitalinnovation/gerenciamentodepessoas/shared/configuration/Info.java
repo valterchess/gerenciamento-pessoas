d2e8dbb4c1e5fc49e0be2ca384de9676270b3a72
valterchess <valterrekabano7@gmail.com>
2022-02-06 21:42:40 -0300

    adicionando a licença apache 2.0 | Editando Info.java em shared

--- src/main/java/one/digitalinnovation/gerenciamentodepessoas/shared/configuration/Info.java
+++ src/main/java/one/digitalinnovation/gerenciamentodepessoas/shared/configuration/Info.java
@@ -3,27 +3,27 @@ package one.digitalinnovation.gerenciamentodepessoas.shared.configuration;
 public class Info {
 
     //api()
-    protected static final String BASE_PACK = "one.digitalinnovation.gerenciamentodepessoas.api.controller";
+    public static final String BASE_PACK = "one.digitalinnovation.gerenciamentodepessoas.api.controller";
 
     //metadata()
-    protected static final String TITULO = "API - Gerenciamento de pessoas";
-    protected static final String DESCRICAO = "Projeto API Spring - Gerenciamento de Pessoas";
-    protected static final String VERSAO = "1.4.1";
-    protected static final String LICENCE = "Apache License Version 2.0";
-    protected static final String LICENCE_URL = "https://github.com/tholivera";
+    public static final String TITULO = "API - Gerenciamento de pessoas";
+    public static final String DESCRICAO = "Projeto API Spring - Gerenciamento de Pessoas";
+    public static final String VERSAO = "1.5.1";
+    public static final String LICENCE = "Apache License Version 2.0";
+    public static final String LICENCE_URL = "https://github.com/tholivera";
 
     //contact()
-    protected static final String NOME = "Valter Silva e Thaís Oliveira";
-    protected static final String GITHUB = "https://github.com/tholivera";
-    protected static final String EMAIL = "valter.silva.dev@gmail.com";
+    public static final String NOME = "Valter Silva e Thaís Oliveira";
+    public static final String GITHUB = "https://github.com/tholivera";
+    public static final String EMAIL = "valter.silva.dev@gmail.com";
 
     //responseMessage()
-    protected static final String SUCESSO = "Sucesso!";
-    protected static final String CRIADO = "Criado!";
-    protected static final String ERRO_NA_REQUISICAO = "Erro na requisição!";
-    protected static final String NAO_AUTORIZADO = "Não autorizado!";
-    protected static final String PROIBIDO = "Proibido!";
-    protected static final String NAO_ENCONTRADO = "Não encontrado";
-    protected static final String ERRO = "Erro!";
-    protected static final String REMOVIDO = "\'Removido!\'";
+    public static final String SUCESSO = "Sucesso!";
+    public static final String CRIADO = "Criado!";
+    public static final String ERRO_NA_REQUISICAO = "Erro na requisição!";
+    public static final String PROIBIDO = "Proibido!";
+    public static final String NAO_ENCONTRADO = "Não encontrado";
+    public static final String NAO_AUTORIZADO = "Não autorizado!";
+    public static final String ERRO = "Erro!";
+    public static final String REMOVIDO = "'Removido!'";
 }
